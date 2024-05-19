423427d2b3cde78eb6155ccf5a2fa072b15e92c9
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-19 15:12:29 -0300

    feat: inserted bottom-navigation in Index.routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,12 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 
 // contexto
 import { StackRoutes } from './stack.routes'
+import BottomTabsRoutes from './bottom-tabs.routes'
 
 // Caixa de contextos
 export function Routes() {
     return (
         <NavigationContainer>
-            <StackRoutes />
+            <BottomTabsRoutes/>
         </NavigationContainer>
     )
 }
