0dcbc0b3fd8d9869c7af087ff5223e6cdcc3938c
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-19 01:49:22 -0300

    feat: Created component Gallery

---
+++ src/screens/GalleryScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const GalleryScreen = () => {
+  return (
+    <View>
+        <Text>Galeria</Text>
+    </View>
+  )
+}
+
+export default GalleryScreen
