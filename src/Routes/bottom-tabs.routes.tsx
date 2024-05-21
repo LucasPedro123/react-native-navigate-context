91d248bd23d486ba84f1bd43fdb5a7725461ed14
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-21 01:36:41 -0300

    feat: insert context provider

--- src/Routes/bottom-tabs.routes.tsx
+++ src/Routes/bottom-tabs.routes.tsx
@@ -2,36 +2,39 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/homeScreen';
 import GalleryScreen from '../screens/GalleryScreen';
 import { MaterialCommunityIcons, Feather } from '@expo/vector-icons'
+import { UserContextProvider } from '../contexts/userContext';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabsRoutes() {
     return (
-        <Tab.Navigator >
-            <Tab.Screen name='Home' component={HomeScreen}
-                options={{
-                    title: 'Menu Principal',
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons
-                            name='home'
-                            color='blue'
-                            size={size}
-                        />
-                    )
-                }} />
-            <Tab.Screen name='Gallery' component={GalleryScreen}
-                options={{
-                    title: 'Galeria',
-                    tabBarIcon: ({ color, size }) => (
-                        <Feather
-                            name='video'
-                            color='blue'
-                            size={size}
-                        />
-                    )
-                }}
-            />
-        </Tab.Navigator>
+        <UserContextProvider>
+            <Tab.Navigator >
+                <Tab.Screen name='Home' component={HomeScreen}
+                    options={{
+                        title: 'Menu Principal',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name='home'
+                                color='blue'
+                                size={size}
+                            />
+                        )
+                    }} />
+                <Tab.Screen name='Gallery' component={GalleryScreen}
+                    options={{
+                        title: 'Galeria',
+                        tabBarIcon: ({ color, size }) => (
+                            <Feather
+                                name='video'
+                                color='blue'
+                                size={size}
+                            />
+                        )
+                    }}
+                />
+            </Tab.Navigator>
+        </UserContextProvider>
     )
 }
 
